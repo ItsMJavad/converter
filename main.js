8eb96f016e12a9481cb51a52fdd9f2ffdb34f0fa
Javad nik Bakhsh <itsmjavad@gmail.com>
2025-10-09 13:34:54 +0330

    Adding Decimal to Binary Converter

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const input = document.getElementById("input");
 const resultP = document.getElementById("result");
+const changer = document.getElementById("changer");
+const header = document.getElementById("header");
 function toDecimal(binary) {
     let y = 0;
     binary = binary.toString();
@@ -19,6 +21,41 @@ function toDecimal(binary) {
     }
     return y;
 }
+function toBinary(decimal) {
+    if (decimal <= 0) {
+        return 0;
+    } else {
+        let answer = Math.floor(decimal / 2);
+        let remain = decimal % 2;
+        let y = remain.toString();
+        while (answer != 0) {
+            remain = (answer % 2).toString();
+            answer = Math.floor(answer / 2);
+            y = remain + y;
+        }
+        return Number(y);
+    }
+}
+changer.addEventListener("click", () => {
+    header.classList.toggle("active");
+    input.value = "";
+    resultP.textContent = "";
+    if (header.classList.contains("active")) {
+        input.placeholder = "Enter the Decimal Number...";
+        header.textContent = "Decimal to Binary Converter";
+        input.addEventListener("input", function () {
+            const result = toBinary(input.value);
+            resultP.textContent = result;
+        })
+    } else {
+        input.placeholder = "Enter the Binary Number";
+        header.textContent = "Binary to Decimal Converter";
+        input.addEventListener("input", function () {
+            const result = toDecimal(input.value);
+            resultP.textContent = result;
+        })
+    }
+})
 input.addEventListener("input", function () {
     const result = toDecimal(input.value);
     resultP.textContent = result;
